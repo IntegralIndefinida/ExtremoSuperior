2ce6407b16f0100ff6b08bbb58a423f8c563a68d
IntegralIndefinida <luusarmandoramirez@hotmail.com>
2024-08-03 10:57:59 -0500

    Update Adivina adivinador.py

--- Idk/Adivina adivinador.py
+++ Idk/Adivina adivinador.py
@@ -2,13 +2,15 @@ import random as random
 
 print("¡Intenta adivinar mi número!")
 x = random.randrange(1,10)
-print(x)
 a = int(input("Elige un número del 1 al 10..."))
-while a != x:
-    print("Lo siento, te has equivocado.")
-    a = int(input("Intenta de nuevo. Elige un número del 1 al 10..."))
-    if a == x:
-        break
 if a == x:
     print("¡Correcto!")
+else:
+    while a != x:
+        print("Lo siento, te has equivocado.")
+        a = int(input("Intenta de nuevo. Elige un número del 1 al 10..."))
+        if a == x:
+            print("¡Correcto!")
+
+        
 
